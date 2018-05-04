4c586a0c39e48327519991899c2aade586692309
Suraj Kumar <surajknair22@gmail.com>
2018-05-04 12:14:55 -0400

    First Checkin

---
+++ Twitter-Mock/javascripts/tweetApp.js
@@ -0,0 +1,13 @@
+//tweetApp.js
+var app = angular.module('tweetApp', []);
+
+app.controller('mainController', function($scope){
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+  
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+});
