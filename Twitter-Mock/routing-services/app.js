970d6eff5854029bd0ab2f7cae1f9ba20e90ee26
Suraj Kumar <surajknair22@gmail.com>
2018-05-11 11:30:15 -0400

    Integration with Express and MongoDB.
    Second part of the commit is to build the integration between api's and mongodb.

---
+++ Twitter-Mock/routing-services/app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+
+var api = require('./routes/api');
+var authentication = require('./routes/authentication')(passport);
+
+//database
+var mongoose = require('mongoose');
+//Connect to mongo db.
+mongoose.connect("mongodb://localhost:27017/tweet-test");
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret'
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Initialize models.
+require('./models/models.js');
+
+//Initialising Passport Lib Strategy for user authentication.
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+
+app.use('/api', api);
+app.use('/auth', authentication);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
